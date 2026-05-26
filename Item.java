f7b080eb3a895e6548b6025fdf7081d71316a300
s166541-svg <s166541@scusd.net>
2026-05-26 13:21:08 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,19 +2,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 // --- FILE: Item.java ---
 /*
-* Represents a generic item in the game.
-* This class serves as the base class for all collectible or interactive entities
-* within the game world. It is utilized by Room and Player classes to manage collections
-* of items using standard ArrayList structures.
+Base class representing an item in the game world.
+
+This class serves as the root of the item hierarchy and is extended by specialized item types.
+
+It interacts with Room and Player classes to represent physical entities.
 */
 public class Item {
 private String name;
 private String description;
 
 /*
-* Constructs an Item with a name and description.
-* @param name the unique name of the item
-* @param description the detailed flavor text of the item
+Constructs an Item with a specified name and description.
+
+@param name the unique name of the item
+
+@param description the verbose description of what the item does or looks like
 */
 public Item(String name, String description) {
 this.name = name;
@@ -22,27 +25,30 @@ this.description = description;
 }
 
 /*
-* Gets the name of the item.
-* @return the name of the item
+Gets the name of the item.
+
+@return the item name string
 */
 public String getName() {
-return this.name;
+return name;
 }
 
 /*
-* Gets the description of the item.
-* @return the item description
+Gets the description of the item.
+
+@return the item description text
 */
 public String getDescription() {
-return this.description;
+return description;
 }
 
 /*
-* Returns the item's name as its string representation.
-* @return the name of the item
+Overrides the toString method to directly print the item's name.
+
+@return the name of the item
 */
 @Override
 public String toString() {
-return this.name;
+return name;
 }
 }
