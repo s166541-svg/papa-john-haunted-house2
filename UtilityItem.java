8f0c58dbb5d167ad1e09e25b835dc9d5fbdb8e52
s166541-svg <s166541@scusd.net>
2026-05-26 13:22:26 -0700

    Update UtilityItem.java

--- UtilityItem.java
+++ UtilityItem.java
@@ -1,17 +1,15 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 // --- FILE: UtilityItem.java ---
-/
-
+/*
 Represents a utility item used for puzzle progression or environmental illumination.
 
 Demonstrates inheritance by extending Item and providing specific context for progression objects.
 
 Used by Player and Game to bypass physical barriers like dark rooms or heavy locks.
-/
+*/
 public class UtilityItem extends Item {
 /*
-
 Constructs a UtilityItem by routing fields to the parent constructor.
 
 @param name the descriptive name of the utility object
