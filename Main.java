a6b98ca76f434f512e30921be846a22550b89ea8
s166541-svg <s166541@scusd.net>
2026-05-27 12:50:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,16 @@
 // --- FILE: Main.java ---
 /*
 
-Entry point for Papa John's Haunted House.
+Entry point for the Papa John's Haunted House game.
 
-This class serves as the starting point of the application, responsible for
-
-instantiating the Game class and initiating the game loop.
+This class instantiates the Game and begins the execution loop.
 */
 public class Main {
 /*
 
-Main method that launches the game.
+Main method to start the program.
 
-@param args Command line arguments (not used).
+@param args Command line arguments
 */
 public static void main(String[] args) {
 Game game = new Game();
