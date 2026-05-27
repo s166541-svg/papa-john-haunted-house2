95101b083d8942c21fea1fee27890408cc5d5770
s226860-droid <s226860@scusd.net>
2026-05-27 13:12:27 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,7 +2,6 @@
 import java.util.ArrayList;
 
 /*
-
 Represents the human player in the game.
 
 This class tracks the player's current location and inventory.
@@ -11,18 +10,16 @@ It provides methods for item management and inventory searching.
 */
 public class Player {
 private Room currentRoom;
-private ArrayList inventory;
+private ArrayList<Item> inventory;
 
 /*
-
 Constructs a new Player and initializes an empty inventory.
 */
 public Player() {
-this.inventory = new ArrayList<>();
+this.inventory = new ArrayList<Item>();
 }
 
 /*
-
 Adds an item to the player's inventory.
 
 @param item The Item to be added.
@@ -32,13 +29,11 @@ inventory.add(item);
 }
 
 /*
-
 Removes an item from the player's inventory by name.
 
 @param itemName The name of the item to remove.
 */
 public void removeItem(String itemName) {
-// Manual loop required - built-in search methods are not allowed per AP CS A constraints
 for (int i = 0; i < inventory.size(); i++) {
 if (inventory.get(i).getName().equalsIgnoreCase(itemName)) {
 inventory.remove(i);
@@ -48,7 +43,6 @@ return;
 }
 
 /*
-
 Finds an item in the player's inventory using a for-each loop.
 
 @param itemName The name of the item to search for.
@@ -56,7 +50,6 @@ Finds an item in the player's inventory using a for-each loop.
 @return The Item object if found, otherwise null.
 */
 public Item findItem(String itemName) {
-// Manual loop required - built-in search methods are not allowed per AP CS A constraints
 for (Item item : inventory) {
 if (item.getName().equalsIgnoreCase(itemName)) {
 return item;
@@ -66,7 +59,6 @@ return null;
 }
 
 /*
-
 Sets the player's current location.
 
 @param room The Room the player is moving into.
@@ -76,7 +68,6 @@ this.currentRoom = room;
 }
 
 /*
-
 Gets the player's current location.
 
 @return The current Room object.
@@ -86,12 +77,11 @@ return currentRoom;
 }
 
 /*
-
 Gets the player's inventory list.
 
 @return An ArrayList of Items.
 */
-public ArrayList getInventory() {
+public ArrayList<Item> getInventory() {
 return inventory;
 }
 }
